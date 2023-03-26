81f919eba1ac30a3d1ba4543bbc05b19c9c6cd50
Brad Hards <bradh@frogmouth.net>
2023-03-26 11:14:35 +1100

    siff: partial metadata writing support

--- tools/src/test/java/net/frogmouth/rnd/eofff/tools/siff/CreateFileTest.java
+++ tools/src/test/java/net/frogmouth/rnd/eofff/tools/siff/CreateFileTest.java
@@ -22,6 +22,7 @@ import java.awt.image.WritableRaster;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
 import java.nio.channels.SeekableByteChannel;
@@ -29,7 +30,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import javax.imageio.ImageIO;
 import net.frogmouth.rnd.eofff.imagefileformat.extensions.properties.AbstractItemProperty;
 import net.frogmouth.rnd.eofff.imagefileformat.extensions.properties.AssociationEntry;
@@ -71,6 +75,34 @@ import net.frogmouth.rnd.eofff.uncompressed.uncc.Interleaving;
 import net.frogmouth.rnd.eofff.uncompressed.uncc.SamplingType;
 import net.frogmouth.rnd.eofff.uncompressed.uncc.UncompressedFrameConfigBox;
 import net.frogmouth.rnd.eofff.yuv.Y4mReader;
+import org.jmisb.api.common.KlvParseException;
+import org.jmisb.api.klv.BerDecoder;
+import org.jmisb.api.klv.BerField;
+import org.jmisb.api.klv.IKlvKey;
+import org.jmisb.api.klv.IKlvValue;
+import org.jmisb.api.klv.IMisbMessage;
+import org.jmisb.api.klv.INestedKlvValue;
+import org.jmisb.api.klv.UniversalLabel;
+import org.jmisb.api.klv.st0603.NanoPrecisionTimeStamp;
+import org.jmisb.api.klv.st1204.CoreIdentifier;
+import org.jmisb.core.klv.ArrayUtils;
+import org.jmisb.mimd.st1902.MimdId;
+import org.jmisb.mimd.st1902.MimdIdReference;
+import org.jmisb.mimd.st1903.MIMD;
+import org.jmisb.mimd.st1903.MIMD_SecurityOptions;
+import org.jmisb.mimd.st1903.MIMD_Version;
+import org.jmisb.mimd.st1903.Security;
+import org.jmisb.mimd.st1903.Security_Classification;
+import org.jmisb.mimd.st1903.Security_ClassifyingMethod;
+import org.jmisb.st1603.localset.CorrectionMethod;
+import org.jmisb.st1603.localset.ITimeTransferValue;
+import org.jmisb.st1603.localset.ReferenceSource;
+import org.jmisb.st1603.localset.ST1603DocumentVersion;
+import org.jmisb.st1603.localset.TimeTransferKey;
+import org.jmisb.st1603.localset.TimeTransferLocalSet;
+import org.jmisb.st1603.localset.TimeTransferMethod;
+import org.jmisb.st1603.localset.TimeTransferParameters;
+import org.jmisb.st1603.nanopack.NanoTimeTransferPack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -79,6 +111,14 @@ public class CreateFileTest {
     private static final Logger LOG = LoggerFactory.getLogger(CreateFileTest.class);
     private static final long MAIN_ITEM_ID = 0x1777;
     private static final long REGION_ITEM_ID = 0x1788;
+    private static final long FILE_METADATA_ITEM_ID = 0x1902;
+    private static final String MIMD_URI = "urn:nsg.KLV.ul:[060E2B34.02050101.0E010504.00000000]";
+    private static final long PRIMARY_TIMESTAMP_ITEM_ID = 0x1603;
+    private static final String NANO_TIMESTAMP_URI =
+            "urn:nsg.KLV.ul:[060E2B34.02050101.0E010302.09000000]";
+    private static final long CORE_ID_ITEM_ID = 0x1204;
+    private static final String CORE_ID_URI =
+            "urn:nsg.KLV.ul:[060E2B34.01010101.0E010405.03000000]";
     private static final int IMAGE_WIDTH = 1280;
     private static final int IMAGE_HEIGHT = 720;
     private static final int NUM_BYTES_PER_PIXEL_RGB = 3;
@@ -103,6 +143,34 @@ public class CreateFileTest {
                 Color.ORANGE,
                 Color.GRAY
             };
+
+    private static final byte[] MIIS_UUID_BYTES =
+            new byte[] {
+                (byte) 0x36,
+                (byte) 0x41,
+                (byte) 0xa9,
+                (byte) 0xe2,
+                (byte) 0x62,
+                (byte) 0x4f,
+                (byte) 0x43,
+                (byte) 0x46,
+                (byte) 0x8f,
+                (byte) 0x8e,
+                (byte) 0x66,
+                (byte) 0x6a,
+                (byte) 0x9b,
+                (byte) 0xaa,
+                (byte) 0x08,
+                (byte) 0xbd
+            };
+    private final UUID miisUuid;
+
+    public CreateFileTest() {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(MIIS_UUID_BYTES);
+        long high = byteBuffer.getLong();
+        long low = byteBuffer.getLong();
+        miisUuid = new UUID(high, low);
+    }
 
     @Test
     public void writeFile_rgb3() throws IOException {
@@ -614,13 +682,45 @@ public class CreateFileTest {
 
     private ItemInfoBox makeItemInfoBox_with_rgan() {
         ItemInfoBox iinf = makeItemInfoBox();
-        ItemInfoEntry infe1 = new ItemInfoEntry();
-        infe1.setVersion(2);
-        infe1.setItemID(REGION_ITEM_ID);
-        FourCC rgan = new FourCC("rgan");
-        infe1.setItemType(rgan.asUnsigned());
-        infe1.setItemName("Region Annotation");
-        iinf.addItem(infe1);
+        {
+            ItemInfoEntry infe1 = new ItemInfoEntry();
+            infe1.setVersion(2);
+            infe1.setItemID(REGION_ITEM_ID);
+            FourCC rgan = new FourCC("rgan");
+            infe1.setItemType(rgan.asUnsigned());
+            infe1.setItemName("Region Annotation");
+            iinf.addItem(infe1);
+        }
+        {
+            ItemInfoEntry infe2 = new ItemInfoEntry();
+            infe2.setVersion(2);
+            infe2.setItemID(FILE_METADATA_ITEM_ID);
+            FourCC uri_fourcc = new FourCC("uri ");
+            infe2.setItemType(uri_fourcc.asUnsigned());
+            infe2.setItemUriType(MIMD_URI);
+            infe2.setItemName("File metadata (MIMD)");
+            iinf.addItem(infe2);
+        }
+        {
+            ItemInfoEntry infe3 = new ItemInfoEntry();
+            infe3.setVersion(2);
+            infe3.setItemID(PRIMARY_TIMESTAMP_ITEM_ID);
+            FourCC uri_fourcc = new FourCC("uri ");
+            infe3.setItemType(uri_fourcc.asUnsigned());
+            infe3.setItemUriType(NANO_TIMESTAMP_URI);
+            infe3.setItemName("Nano timestamp metadata (ST 1603)");
+            iinf.addItem(infe3);
+        }
+        {
+            ItemInfoEntry infe4 = new ItemInfoEntry();
+            infe4.setVersion(2);
+            infe4.setItemID(CORE_ID_ITEM_ID);
+            FourCC uri_fourcc = new FourCC("uri ");
+            infe4.setItemType(uri_fourcc.asUnsigned());
+            infe4.setItemUriType(CORE_ID_URI);
+            infe4.setItemName("Core ID metadata (ST 1204)");
+            iinf.addItem(infe4);
+        }
         return iinf;
     }
 
@@ -634,21 +734,65 @@ public class CreateFileTest {
 
     private ItemLocationBox makeItemLocationBox_rgb_component_with_rgan() throws IOException {
         ItemLocationBox iloc = makeItemLocationBox_rgb3();
-        ILocItem regionItemLocation = new ILocItem();
-        regionItemLocation.setConstructionMethod(1);
-        regionItemLocation.setItemId(REGION_ITEM_ID);
-        ILocExtent rganExtent = new ILocExtent();
-        rganExtent.setExtentIndex(0);
-        rganExtent.setExtentOffset(0);
-        rganExtent.setExtentLength(getRegionAnnotationBytes().length);
-        regionItemLocation.addExtent(rganExtent);
-        iloc.addItem(regionItemLocation);
+        {
+            ILocItem regionItemLocation = new ILocItem();
+            regionItemLocation.setConstructionMethod(1);
+            regionItemLocation.setItemId(REGION_ITEM_ID);
+            ILocExtent rganExtent = new ILocExtent();
+            rganExtent.setExtentIndex(0);
+            rganExtent.setExtentOffset(0);
+            rganExtent.setExtentLength(getRegionAnnotationBytes().length);
+            regionItemLocation.addExtent(rganExtent);
+            iloc.addItem(regionItemLocation);
+        }
+        {
+            ILocItem mimdItemLocation = new ILocItem();
+            mimdItemLocation.setConstructionMethod(1);
+            mimdItemLocation.setItemId(FILE_METADATA_ITEM_ID);
+            ILocExtent mimdExtent = new ILocExtent();
+            mimdExtent.setExtentIndex(0);
+            mimdExtent.setExtentOffset(getRegionAnnotationBytes().length);
+            mimdExtent.setExtentLength(getFileMetadataBytes(false).length);
+            mimdItemLocation.addExtent(mimdExtent);
+            iloc.addItem(mimdItemLocation);
+        }
+        {
+            ILocItem nanoTimestampItemLocation = new ILocItem();
+            nanoTimestampItemLocation.setConstructionMethod(1);
+            nanoTimestampItemLocation.setItemId(PRIMARY_TIMESTAMP_ITEM_ID);
+            ILocExtent nanoTimestampExtent = new ILocExtent();
+            nanoTimestampExtent.setExtentIndex(0);
+            nanoTimestampExtent.setExtentOffset(
+                    getRegionAnnotationBytes().length + getFileMetadataBytes(false).length);
+            nanoTimestampExtent.setExtentLength(getNanoTimestampBytes(false).length);
+            nanoTimestampItemLocation.addExtent(nanoTimestampExtent);
+            iloc.addItem(nanoTimestampItemLocation);
+        }
+        {
+            ILocItem coreIdItemLocation = new ILocItem();
+            coreIdItemLocation.setConstructionMethod(1);
+            coreIdItemLocation.setItemId(CORE_ID_ITEM_ID);
+            ILocExtent coreIdExtent = new ILocExtent();
+            coreIdExtent.setExtentIndex(0);
+            coreIdExtent.setExtentOffset(
+                    getRegionAnnotationBytes().length
+                            + getFileMetadataBytes(false).length
+                            + getNanoTimestampBytes(false).length);
+            coreIdExtent.setExtentLength(this.getMiisCoreIdAsBytes(false).length);
+            coreIdItemLocation.addExtent(coreIdExtent);
+            iloc.addItem(coreIdItemLocation);
+        }
         return iloc;
     }
 
     private ItemDataBox makeItemDataBox_with_rgan() throws IOException {
         ItemDataBox idat = new ItemDataBox();
-        idat.setData(this.getRegionAnnotationBytes());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write(this.getRegionAnnotationBytes());
+        baos.write(this.getFileMetadataBytes(true));
+        baos.write(this.getNanoTimestampBytes(true));
+        baos.write(this.getMiisCoreIdAsBytes(true));
+        idat.setData(baos.toByteArray());
         return idat;
     }
 
@@ -878,11 +1022,27 @@ public class CreateFileTest {
 
     private Box makeItemReferenceBox_with_rgan() {
         ItemReferenceBox iref = new ItemReferenceBox();
-        SingleItemReferenceBox rgan_csdc_primary_item =
-                new SingleItemReferenceBox(new FourCC("cdsc"));
-        rgan_csdc_primary_item.setFromItemId(REGION_ITEM_ID);
-        rgan_csdc_primary_item.addReference(MAIN_ITEM_ID);
-        iref.addItem(rgan_csdc_primary_item);
+        {
+            SingleItemReferenceBox rgan_csdc_primary_item =
+                    new SingleItemReferenceBox(new FourCC("cdsc"));
+            rgan_csdc_primary_item.setFromItemId(REGION_ITEM_ID);
+            rgan_csdc_primary_item.addReference(MAIN_ITEM_ID);
+            iref.addItem(rgan_csdc_primary_item);
+        }
+        {
+            SingleItemReferenceBox timestamp_csdc_primary_item =
+                    new SingleItemReferenceBox(new FourCC("cdsc"));
+            timestamp_csdc_primary_item.setFromItemId(PRIMARY_TIMESTAMP_ITEM_ID);
+            timestamp_csdc_primary_item.addReference(MAIN_ITEM_ID);
+            iref.addItem(timestamp_csdc_primary_item);
+        }
+        {
+            SingleItemReferenceBox coreId_csdc_primary_item =
+                    new SingleItemReferenceBox(new FourCC("cdsc"));
+            coreId_csdc_primary_item.setFromItemId(CORE_ID_ITEM_ID);
+            coreId_csdc_primary_item.addReference(MAIN_ITEM_ID);
+            iref.addItem(coreId_csdc_primary_item);
+        }
         return iref;
     }
 
@@ -2842,5 +3002,122 @@ public class CreateFileTest {
         OutputStreamWriter streamWriter = new OutputStreamWriter(baos);
         rgan.writeTo(streamWriter);
         return baos.toByteArray();
+    }
+
+    private byte[] getFileMetadataBytes(boolean dump) throws IOException {
+        try {
+            MIMD mimd = new MIMD();
+            mimd.setVersion(new MIMD_Version(1));
+            mimd.setSecurityOptions(buildSecurityOptions());
+            mimd.setCompositeProductSecurity(
+                    new MimdIdReference(
+                            SECURITY_ID_SERIAL,
+                            SECURITY_ID_GROUP,
+                            "CompositeProductSecurity",
+                            "Security"));
+            if (dump) {
+                dumpMisbMessage(mimd);
+            }
+            byte[] mimdBytes = mimd.frameMessage(false);
+            BerField ber = BerDecoder.decode(mimdBytes, UniversalLabel.LENGTH, false);
+            int lengthOfLength = ber.getLength();
+            byte[] mimdValueBytes = new byte[ber.getValue()];
+            System.arraycopy(
+                    mimdBytes,
+                    UniversalLabel.LENGTH + lengthOfLength,
+                    mimdValueBytes,
+                    0,
+                    mimdValueBytes.length);
+            return mimdValueBytes;
+        } catch (KlvParseException ex) {
+            throw new IOException(ex.toString());
+        }
+    }
+
+    private byte[] getNanoTimestampBytes(boolean dump) throws IOException {
+        long nanos = System.currentTimeMillis() * 1000 * 1000;
+        NanoPrecisionTimeStamp timestamp = new NanoPrecisionTimeStamp(nanos);
+        Map<TimeTransferKey, ITimeTransferValue> localSetValues = new HashMap<>();
+        localSetValues.put(TimeTransferKey.DocumentVersion, new ST1603DocumentVersion(2));
+        localSetValues.put(
+                TimeTransferKey.TimeTransferParameters,
+                new TimeTransferParameters(
+                        ReferenceSource.Unknown,
+                        CorrectionMethod.Unknown,
+                        TimeTransferMethod.Unknown));
+        TimeTransferLocalSet localSet = new TimeTransferLocalSet(localSetValues);
+        NanoTimeTransferPack nanoPack = new NanoTimeTransferPack(timestamp, localSet);
+        if (dump) {
+            dumpMisbMessage(nanoPack);
+        }
+        return nanoPack.frameMessage(false);
+    }
+
+    private byte[] getMiisCoreIdAsBytes(boolean dump) {
+        CoreIdentifier miisCoreId = new CoreIdentifier();
+        miisCoreId.setMinorUUID(miisUuid);
+        miisCoreId.setVersion(1);
+        if (dump) {
+            System.out.println("CoreID: " + miisCoreId.getTextRepresentation());
+        }
+        byte[] miisCoreIdAsBytes = miisCoreId.getRawBytesRepresentation();
+        return miisCoreIdAsBytes;
+    }
+
+    private static final int SECURITY_ID_GROUP = 1;
+    private static final int SECURITY_ID_SERIAL = 2;
+
+    private static MIMD_SecurityOptions buildSecurityOptions() throws KlvParseException {
+        Security security = new Security();
+        MimdId securityId = new MimdId(SECURITY_ID_SERIAL, SECURITY_ID_GROUP);
+        security.setMimdId(securityId);
+        security.setClassification(new Security_Classification("UNCLASSIFIED//"));
+        security.setClassifyingMethod(new Security_ClassifyingMethod("US-1"));
+        List<Security> securities = new ArrayList<>();
+        securities.add(security);
+        MIMD_SecurityOptions securityOptions = new MIMD_SecurityOptions(securities);
+        return securityOptions;
+    }
+
+    private static void dumpMisbMessage(IMisbMessage mimd) {
+        outputTopLevelMessageHeader(mimd);
+        outputNestedKlvValue(mimd, 1);
+    }
+
+    private static void outputTopLevelMessageHeader(IMisbMessage misbMessage) {
+        String displayHeader = misbMessage.displayHeader();
+        if (displayHeader.equalsIgnoreCase("Unknown")) {
+            System.out.println(
+                    displayHeader
+                            + " ["
+                            + ArrayUtils.toHexString(misbMessage.getUniversalLabel().getBytes())
+                                    .trim()
+                            + "]");
+            outputUnknownMessageContent(misbMessage.frameMessage(true));
+        } else {
+            System.out.println(displayHeader);
+        }
+    }
+
+    private static void outputUnknownMessageContent(byte[] frameMessage) {
+        System.out.println(ArrayUtils.toHexString(frameMessage, 16, true));
+    }
+
+    private static void outputNestedKlvValue(INestedKlvValue nestedKlvValue, int indentationLevel) {
+        for (IKlvKey identifier : nestedKlvValue.getIdentifiers()) {
+            IKlvValue value = nestedKlvValue.getField(identifier);
+            outputValueWithIndentation(value, indentationLevel);
+            // if this has nested content, output that at the next indentation level
+            if (value instanceof INestedKlvValue) {
+                outputNestedKlvValue((INestedKlvValue) value, indentationLevel + 1);
+            }
+        }
+    }
+
+    private static void outputValueWithIndentation(IKlvValue value, int indentationLevel) {
+        for (int i = 0; i < indentationLevel; ++i) {
+            System.out.print("\t");
+        }
+        System.out.println(value.getDisplayName() + ": " + value.getDisplayableValue());
     }
 }
