2a7026c5466924fbb27c32f11b825b2d2c2b736e
Brad Hards <bradh@frogmouth.net>
2023-04-10 11:07:56 +1000

    imagefileformat: make item property interface more like box

--- imagefileformat/src/main/java/net/frogmouth/rnd/eofff/imagefileformat/extensions/properties/AbstractItemProperty.java
+++ imagefileformat/src/main/java/net/frogmouth/rnd/eofff/imagefileformat/extensions/properties/AbstractItemProperty.java
@@ -1,10 +1,15 @@
 package net.frogmouth.rnd.eofff.imagefileformat.extensions.properties;
 
 import java.io.IOException;
+import net.frogmouth.rnd.eofff.isobmff.FourCC;
 import net.frogmouth.rnd.eofff.isobmff.OutputStreamWriter;
 
 public interface AbstractItemProperty {
     public abstract void writeTo(OutputStreamWriter writer) throws IOException;
 
     public abstract long getSize();
+
+    public String getFullName();
+
+    FourCC getFourCC();
 }
